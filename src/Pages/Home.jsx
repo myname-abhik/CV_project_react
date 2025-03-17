51f478a2d264722d050f947a2b2dfc7b5ee387ab
myname-abhik <abhik16chakrabortty@gmail.com>
2025-03-17 14:59:20 +0530

    second-commite

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,14 +8,35 @@ import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 
 import 'swiper/css';
 import 'swiper/css/pagination';
+import "swiper/css/navigation";
 import Crousal from '../component/Crousal';
 import movies from '../assets/Movies_info/Movies_info.json'
 import { Movie } from '@mui/icons-material';
 import Movies from '../component/Movies';
 import movie_pickups from '../assets/Movies_info/Movie_pickup.json'
+import Lenis from "@studio-freight/lenis";
 
 
 const Home = () => {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.9, // Adjusts smoothness
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Custom easing
+      direction: "vertical", // Can be "horizontal" as well
+      smooth: true,
+      smoothTouch: false, // Set true if you want smooth scrolling on touch devices
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => lenis.destroy(); // Cleanup on unmount
+  }, []);
+
 
 const pagref = useRef(null)
   return (
@@ -55,7 +76,7 @@ const pagref = useRef(null)
                <div className="swiper-wrapper Copy_Swiper">
                {
                 movies.map((movie,index)=>(
-                  <SwiperSlide  key={index}>
+                  <SwiperSlide  >
                 <Crousal image={movie.Poster} ele={movie.Movie}Description={movie.Description} genre={movie.Genres}/>
                 </SwiperSlide>
                ))}
@@ -68,25 +89,36 @@ const pagref = useRef(null)
              
        </div>
 
-       <div className="Lower_frame_Top_Pickups">
-      <div className="Title_pickup">Top picks for you <i className="material-icons right_arrow">arrow_right_alt</i></div>
-      <div className="Movie_list">
-      {movie_pickups.map((movie)=>(
-        <Movies image={movie.Movie_poster} features={movie.Movie_feature} title={movie.Movie_title}/>
-      ))}
-        <div className="swiper Second_swiper">
-          <div className="swiper-wrapper Second_swiper-wrapper">
-         
-          </div>
-          <div className="swiper-button-next second_swiper-button-next"></div>
-          <div className="swiper-button-prev second_swiper-button-prev"></div>
-        </div>
-      </div>
-      
-     
 
-    </div>
         </div>
+        <div className="Lower_frame_Top_Pickups">
+       
+       <div className="Title_pickup">Top picks for you <i className="material-icons right_arrow">arrow_right_alt</i></div>
+      
+       <div className="Movie_list">
+       <Swiper
+       modules={[Navigation, Pagination, Autoplay]}
+   loop={true}
+   autoplay={{ delay: 3000, disableOnInteraction: true }}
+   navigation={ true }
+   slidesPerView={5}
+ 
+       
+       >
+      
+       {movie_pickups.map((movie)=>(
+         <SwiperSlide className='second_swiper-slide'>
+         <Movies image={movie.Movie_poster} features={movie.Movie_feature} title={movie.Movie_title}/>
+         </SwiperSlide>
+       ))}
+       
+    
+       </Swiper>
+       </div>
+       
+      
+ 
+     </div>
         </div>
     </>
   )
