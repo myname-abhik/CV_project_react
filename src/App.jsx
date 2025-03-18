4409ba0ca2b76cf109aff7c7a2087dc014f975b5
myname-abhik <abhik16chakrabortty@gmail.com>
2025-03-19 03:42:49 +0530

    res_@

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
   return (
   
     <Router>
-    {/* <Navbar/> */}
+    <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Movies" element={<Mov/>}/>
